acb6f7109ea6b6df02159930977f563489131738
Ronald Head <ronaldlhead@gmail.com>
2026-02-14 13:14:26 -0700

    Add Player class, methods, and update main

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     print("Starting Asteroids with pygame version: VERSION")
@@ -8,15 +9,26 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
 
     while screen:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
         screen.fill("black")
+        for draw in drawable:
+            draw.draw(screen)
+        updatable.update(dt)
         pygame.display.flip()
+        clock.tick(60)
+        dt = clock.tick(60)/1000
+        
+
 
 if __name__ == "__main__":
     main()
