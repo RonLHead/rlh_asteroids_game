056db9daba828247cf7361b214036ae6ace76553
Ronald Head <ronaldlhead@gmail.com>
2026-02-14 23:58:34 -0700

    Add Asteroid, AsteroidField, Shot classes, update methods in player and update main

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import pygame
+import sys
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from logger import log_event
+from shot import Shot
 
 def main():
     print("Starting Asteroids with pygame version: VERSION")
@@ -13,8 +18,14 @@ def main():
     dt = 0
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
+    asteroids = pygame.sprite.Group()
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
+    Shot.containers = (shots, drawable, updatable)
     player_obj = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
 
     while screen:
         log_state()
@@ -23,9 +34,26 @@ def main():
                 return
            
         screen.fill("black")
+
         for draw in drawable:
             draw.draw(screen)
+
         updatable.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player_obj):
+                log_event("player_hit")
+                print("Game Over!")
+                sys.exit()
+
+        for asteroid in asteroids:
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    log_event("asteroid_shot")
+                    asteroid.split()
+                    shot.kill()
+            
+                
         pygame.display.flip()
         clock.tick(60)
         dt = clock.tick(60)/1000
